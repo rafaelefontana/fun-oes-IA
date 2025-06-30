fef8432be92559ad9fe83feeb1f7d2ef0e54fa1d
rafaaaaaaaaa <146213762+rafaelefontana@users.noreply.github.com>
2025-06-30 11:17:31 -0300

    3006

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual é o maior planeta do Sistema Solar?"
+    
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Saturno
+b) Júpiter);",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
